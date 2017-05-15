8354ed98ecdf35651d8973e33cdb0e3a3fd1e445
Ritesh Kadmawala <ritesh@loanzen.in>
2017-05-15 14:51:42 +0530

    added some config from http://whattheemacsd.com/

--- init.el
+++ init.el
@@ -12,6 +12,9 @@
              '("melpa" . "https://melpa.org/packages/") t)
 
 
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") )
+
+
 ;; Loading zenburn
 (load-theme 'zenburn t)
 
@@ -19,6 +22,34 @@
 (setq split-width-threshold 0)
 
 
+;;;;;;;;;;;;;;;;;;;;;;Taken From http://whattheemacsd.com/;;;;;;;;;;;;;;;;;;;;;;
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+;; Webjump Config
+
+(global-set-key (kbd "C-x g") 'webjump)
+
+;; Add Urban Dictionary to webjump
+(eval-after-load "webjump"
+'(add-to-list 'webjump-sites
+              '("Urban Dictionary" .
+                [simple-query
+                 "www.urbandictionary.com"
+                 "http://www.urbandictionary.com/define.php?term="
+                 ""])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (custom-set-variables
@@ -26,7 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (zenburn-theme js2-mode))))
+ '(package-selected-packages (quote (better-defaults zenburn-theme js2-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
