e17c395cc39f19309353e0dc19602e4677428787
Ritesh Kadmawala <ritesh@loanzen.in>
2017-05-15 14:15:01 +0530

    Initial Configuration

---
+++ init.el
@@ -0,0 +1,35 @@
+
+
+(require 'package)
+(package-initialize)
+
+
+;; Adding Melpa as a repository for emacs libs
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+
+;; Loading zenburn
+(load-theme 'zenburn t)
+
+;; Vertical Split by default
+(setq split-width-threshold 0)
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (zenburn-theme js2-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
